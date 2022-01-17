ef604762774dbc34af41701e31608af231ef58a5
Aaron N <Aaron-N@users.noreply.github.com>
2022-01-17 12:56:14 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_portejef',
-  password        : '4220',
-  database        : 'cs361_portejef'
+  user            : '****',
+  password        : '****',
+  database        : '****'
 });
 
 module.exports.pool = pool;
